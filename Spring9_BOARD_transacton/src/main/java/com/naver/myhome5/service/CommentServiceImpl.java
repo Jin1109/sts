a15fd080a1715771baab7ccfd4edc44f1f543050
Jin1109 <jbs107601@naver.com>
2022-09-29 13:27:52 +0900

    Spring9_BOARD_transacton - commit

---
+++ Spring9_BOARD_transacton/src/main/java/com/naver/myhome5/service/CommentServiceImpl.java
@@ -0,0 +1,59 @@
+package com.naver.myhome5.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.naver.myhome5.dao.CommentDAO;
+import com.naver.myhome5.domain.Comment;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+	
+	@Autowired
+	private CommentDAO dao;
+	
+//	@Autowired
+//	private LogAdvice log;
+
+	@Override
+	public int getListCount(int board_num) {
+//		log.beforeLog();
+		return dao.getListCount(board_num);
+	}
+
+	@Override
+	public List<Comment> getCommentList(int board_num, int page) {
+//		log.beforeLog();
+		int startrow=1;
+		int endrow=page*3;
+		
+		Map<String,Integer> map = new HashMap<String,Integer>();
+		map.put("board_num",board_num );
+		map.put("start",startrow);
+		map.put("end", endrow);
+		
+		return dao.getCommentList(map);
+	}
+
+	@Override
+	public int commentsInsert(Comment c) {
+		return dao.commentInsert(c);
+	}
+
+	@Override
+	public int commentsDelete(int num) {
+		return dao.commentDelete(num);
+	}
+
+	@Override
+	public int commentsUpdate(Comment co) {
+		return dao.commentUpdate(co);
+	}
+	
+	
+
+}
---
+++ Spring9_BOARD_transacton/src/main/webapp/resources/sql/delete_file.sql
@@ -0,0 +1,30 @@
+drop table delete_file purge;
+create table delete_file(
+	board_file varchar2(50),
+	reg_date date default sysdate
+);
+
+select * from delete_file;
+
+drop trigger save_delete_file;
+
+select trigger_name from user_triggers;
+
+-- sts에서 실행되는 경우는 15번~27라인(/미포함) 영역 설정 후 Excute Selected Text As One Statement 선택
+-- cmd창에서 실행하는 경우는 15~28라인 복사해서 붙여넣기(/포함)
+create or replace trigger save_delete_file
+after update or delete
+on board2
+ for each row
+ begin
+   if(:old.board_file is not null) then
+    if(:old.board_file != :new.board_file or :new.board_file is null ) then
+	     insert into delete_file
+	      (board_file)
+	     values(:old.board_file);
+     end if;
+   end if;
+ end;
+/
+
+select * from trigger save_delete_file;
